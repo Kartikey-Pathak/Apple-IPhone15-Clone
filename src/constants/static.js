9d26e4fe4f1b73ca624e41a5d32bd2865b2c3e56
Kartikey <kartikeypathak08@gmail.com>
2025-06-17 15:34:00 +0530

    Fixing Folder structure

--- src/constants/static.js
+++ src/constants/static.js
@@ -1,50 +1,39 @@
-export const Slides=[
-    {
-        id:1,
-        texts:[
-            "All-new A17 Pro chip.",
-            "Powers unrivalled intelligence.",
-            "And unprecedented performance."
-        ],
-        video:"./src/assets/highlight-first.mp4",
-        duration:4
-    },
-    {
-       id:2,
-        texts:[
-            "A camera that captures your imagination.",
-            "Closer than ever, even from far away."
-        ],
-        video:"./src/assets/hightlight-sec.mp4",
-        duration:2
-    },
-    {
-     id:3,
-        texts:[
-            "Titanium.",
-            "Max our most powerful iPhone ever."
-        ],
-        video:"./src/assets/iphone-camera-highlight.mp4",
-        duration:5
-    },{
-       id:4,
-        texts:[
-            "The button you can customize.",
-            "Your shortcut to shortcuts."
-        ],
-        video:"./src/assets/btninfo.mp4",
-        duration:3
-    }
-]
-export const navarr = [
-  "Store",
-  "Mac",
-  "iPad",
-  "iPhone",
-  "Watch",
-  "AirPods",
-  "TV&Home",
-  "Entertainment",
-  "Accessories",
-  "Support"
+export const Slides = [
+  {
+    id: 1,
+    texts: [
+      "All-new A17 Pro chip.",
+      "Powers unrivalled intelligence.",
+      "And unprecedented performance."
+    ],
+    video: "/Video/highlight-first.mp4",
+    duration: 4
+  },
+  {
+    id: 2,
+    texts: [
+      "A camera that captures your imagination.",
+      "Closer than ever, even from far away."
+    ],
+    video: "/Video/hightlight-sec.mp4",
+    duration: 2
+  },
+  {
+    id: 3,
+    texts: [
+      "Titanium.",
+      "Max our most powerful iPhone ever."
+    ],
+    video: "/Video/iphone-camera-highlight.mp4",
+    duration: 5
+  },
+  {
+    id: 4,
+    texts: [
+      "The button you can customize.",
+      "Your shortcut to shortcuts."
+    ],
+    video: "/Video/btninfo.mp4",
+    duration: 3
+  }
 ];
