5d261724cf6a332a06a926006a896fcb64027a42
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-06-17 18:17:45 +0530

    Update Game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -69,7 +69,7 @@ function Game(){
             </div>
 
             {/* Gameplay Part */}
-            <div className=" mt-20 w-[99%] h-[10rem] md:w-[80%] md:h-[30rem] flex items-center justify-center relative">
+            <div className=" mt-20 w-[90%] h-[10rem] md:w-[80%] md:h-[30rem] flex items-center justify-center relative">
                 <img src="/img/frame.png" className="z-20 w-full h-full object-contain" alt="" />
                 <video src="/Video/frame.mp4" autoPlay loop muted   playsInline className="  h-[25vh] w-[52vh] md:w-[95%] md:h-[36vw] lg:h-[36vw] xl:h-[28vw] lg:w-[78vw] xl:w-[62vw] object-cover rounded-[2rem] z-10 absolute"></video>
             </div>
