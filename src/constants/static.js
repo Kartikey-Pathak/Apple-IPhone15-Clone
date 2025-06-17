f04715980ab86f5c333c2ecf91a68f9df78ef49d
Kartikey <kartikeypathak08@gmail.com>
2025-06-17 17:42:21 +0530

    Fixed Folder Structure

--- src/constants/static.js
+++ src/constants/static.js
@@ -1,39 +1,49 @@
 export const Slides = [
-  {
-    id: 1,
-    texts: [
-      "All-new A17 Pro chip.",
-      "Powers unrivalled intelligence.",
-      "And unprecedented performance."
-    ],
-    video: "./Video/highlight-first.mp4",
-    duration: 4
-  },
-  {
-    id: 2,
-    texts: [
-      "A camera that captures your imagination.",
-      "Closer than ever, even from far away."
-    ],
-    video: "./Video/hightlight-sec.mp4",
-    duration: 2
-  },
-  {
-    id: 3,
-    texts: [
-      "Titanium.",
-      "Max our most powerful iPhone ever."
-    ],
-    video: "./Video/iphone-camera-highlight.mp4",
-    duration: 5
-  },
-  {
-    id: 4,
-    texts: [
-      "The button you can customize.",
-      "Your shortcut to shortcuts."
-    ],
-    video: "./Video/btninfo.mp4",
-    duration: 3
-  }
+    {
+        id: 1,
+        texts: [
+            "All-new A17 Pro chip.",
+            "Powers unrivalled intelligence.",
+            "And unprecedented performance."
+        ],
+        video: "/Video/highlight-first.mp4",
+        duration: 4
+    },
+    {
+        id: 2,
+        texts: [
+            "A camera that captures your imagination.",
+            "Closer than ever, even from far away."
+        ],
+        video: "/Video/hightlight-sec.mp4",
+        duration: 2
+    },
+    {
+        id: 3,
+        texts: [
+            "Titanium.",
+            "Max our most powerful iPhone ever."
+        ],
+        video: "/Video/iphone-camera-highlight.mp4",
+        duration: 5
+    },
+    {
+        id: 4,
+        texts: [
+            "The button you can customize.",
+            "Your shortcut to shortcuts."
+        ],
+        video: "/Video/btninfo.mp4",
+        duration: 3
+    }
 ];
+export const navarr = ["Store",
+    "Mac",
+    "iPad",
+    "iPhone",
+    "Watch",
+    "AirPods",
+    "TV & Home",
+    "Entertainment",
+    "Accessories",
+    "Support"]
